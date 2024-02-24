13ddd9d653265f52f92b73379758f8677dc46ce4
moncef <moncxff@gmail.com>
2024-02-24 21:39:49 +0100

    few css changes

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import logo from './icon.png'
+import Footer from "./Footer";
 
 export default (props) => {
-    useEffect(()=>{
+    useEffect(() => {
         window.location.href = "#"
-        setTimeout(function(){
+        setTimeout(function () {
             document.querySelector(".Loader").style.opacity = '0'
             document.querySelector(".Loader").style.zIndex = '-1'
         }, (props.time ? props.time : 3000))
@@ -12,6 +13,7 @@ export default (props) => {
     return (
         <div className={`Loader`}>
             <img src={logo} alt="Loader icon" />
+            <Footer style={{ bottom: 0, position: 'fixed' }} />
         </div>
     );
 };
