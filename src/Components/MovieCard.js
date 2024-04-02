1e8ed75ea5a3ea9e83958418a47e081381d755e7
Nemoswrold <154289277+Nemoswrold@users.noreply.github.com>
2024-04-01 23:38:54 -0400

    Update MovieCard.js

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,20 +1,24 @@
 import { useEffect } from "react";
-import { Link } from "react-router-dom";
 
 export default function Card(props) {
-    const imgPath = 'https://image.tmdb.org/t/p/w342'
+    const imgPath = 'https://image.tmdb.org/t/p/w342';
+    
+    useEffect(() => {
+        if (!/null/.test(props.img) && document.querySelector(".recom")) {
+            document.querySelectorAll(".recom").forEach(sct => {
+                sct.scrollTo(0, 0);
+            });
+        }
+    }, [props.img]);
+
     if (!/null/.test(props.img)) {
-        useEffect(() => {
-            if (document.querySelector(".recom")) {
-                document.querySelectorAll(".recom").forEach(sct => {
-                    sct.scrollTo(0, 0)
-                })
-            }
-        })
         return (
             <a href={`/${props.show === 'true' ? 'tv' : 'movie'}/${props.id}`} className="movieCard">
-                <img src={`${imgPath}${props.img}`}></img>
+                <img src={`${imgPath}${props.img}`} alt="Movie Thumbnail"></img>
             </a>
-        )
+        );
     }
+
+    return null;
 }
+```
